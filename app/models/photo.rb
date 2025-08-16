37829618c5204426c1a20a8527ffd4952f5eebc0
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-08-16 08:31:43 +0000

    Added Validation and added default to private

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -27,4 +27,7 @@ class Photo < ApplicationRecord
 
   #Indirect Association
   has_many :fans, through: :likes
+
+  validates :caption, presence: true
+  validates :image, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_16_075142) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_16_082951) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
@@ -207,7 +207,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_16_075142) do
     t.string "avatar_image"
     t.string "bio"
     t.string "website"
-    t.boolean "private"
+    t.boolean "private", default: true
     t.integer "likes_count", default: 0
     t.integer "comments_count", default: 0
     t.datetime "created_at", null: false
